3db15dd10a7bff23a3f3d027ecbca29227ada260
Jenish MS <jenishmstephen@gmail.com>
2021-11-10 09:19:55 +0530

    chart

--- ui/src/app/components/user-country/user-country.component.ts
+++ ui/src/app/components/user-country/user-country.component.ts
@@ -14,10 +14,8 @@ import { AppService } from 'src/app/services/app.service';
 export class UserCountryComponent implements OnInit {
   caseData: Case;
   vaccineData: Vaccine;
-  public lineChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-  ];
-  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartData: ChartDataSets[] = [];
+  public lineChartLabels: Label[] = [];
   public lineChartOptions: ChartOptions = {
     responsive: true,
     maintainAspectRatio: false
@@ -25,17 +23,66 @@ export class UserCountryComponent implements OnInit {
   public lineChartColors: Color[] = [
     {
       borderColor: 'black',
-      backgroundColor: 'rgba(255,0,0,0.3)',
+      // backgroundColor: 'rgba(255,0,0,0.3)',
     },
   ];
   public lineChartLegend = true;
-  public lineChartType = 'line';
+  public lineChartType = 'bar';
   public lineChartPlugins = [];
   constructor(private appService: AppService) { }
 
   async ngOnInit(): Promise<void> {
     await this.getUserCountryCases();
     await this.getUserCountryVaccine();
+    this.getChartData();
+  }
+
+  getChartData() {
+    this.appService.getUserCountryTimeSeries(this.appService.userLocation.region_code).then(response => {
+      if(response.status) {
+        const keys = Object.keys(response.data.dates);
+        this.lineChartLabels = keys;
+        const cases = [];
+        const death = [];
+        const recovered = [];
+        const tested = [];
+        const vaccine1 = [];
+        const vaccine2 = [];
+        const seriesData = response.data?.dates;
+        console.log(seriesData);
+        keys.forEach(data => {
+          if(seriesData[data]) {
+            const dateData = seriesData[data].total;
+            cases.push(dateData?.confirmed ?? 0);
+            death.push(dateData?.deceased ?? 0);
+            recovered.push(dateData?.recovered ?? 0);
+            tested.push(dateData?.tested ?? 0);
+            vaccine1.push(dateData?.vaccinated1 ?? 0);
+            vaccine2.push(dateData?.vaccinated2 ?? 0);
+          }
+          // return { data: [65, 59, 80, 81, 56, 55, 40], label: data }
+        });
+        this.lineChartData = [
+          {
+            label: 'Confirmed',
+            data: cases,
+          },
+          {
+            label: 'Death',
+            data: death,
+          },
+          {
+            label: 'Recovered',
+            data: recovered,
+          },
+          {
+            label: 'Tested',
+            data: tested,
+          }
+        ]
+      }
+    });
+
   }
 
   async getUserCountryCases() {
